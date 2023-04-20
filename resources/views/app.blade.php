26482138f61d07be99271db30c33c5efb9b32a4a
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-04-20 16:13:01 +0200

    remove GA & rename title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,35 +14,7 @@
     <meta name="theme-color" content="#ffffff">
 
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <title>{{ trans('SNG Codes') }}</title>
-    @if (App::environment('production'))
-        {{-- Clarity --}}
-        <script type="text/javascript">
-            (function(c, l, a, r, i, t, y) {
-                c[a] = c[a] || function() {
-                    (c[a].q = c[a].q || []).push(arguments)
-                };
-                t = l.createElement(r);
-                t.async = 1;
-                t.src = "https://www.clarity.ms/tag/" + i;
-                y = l.getElementsByTagName(r)[0];
-                y.parentNode.insertBefore(t, y);
-            })(window, document, "clarity", "script", "az0i6tv0rb");
-        </script>
-        {{-- /Clarity --}}
-        {{-- GA --}}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-222618680-1"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-
-            function gtag() {
-                dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-            gtag('config', 'UA-222618680-1');
-        </script>
-        {{-- /GA --}}
-    @endif
+    <title>Objekt forscher</title>
 </head>
 
 <body>
