a9f02e08ff3aba2bb350df473be422f48375e2e1
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-10-03 19:41:59 +0200

    fix chevron icon sizing

--- resources/js/components/NavigateButton.vue
+++ resources/js/components/NavigateButton.vue
@@ -6,7 +6,7 @@
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-6 h-6 lg:w-32 lg:h-32"
+            class="w-20 h-20 lg:w-32 lg:h-32"
             :class="iconClass"
         >
             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
