7092b6be00fb9bebc2b9964c8ec078fd80b1b459
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-10-03 19:33:26 +0200

    add and use navigate button

---
+++ resources/js/components/NavigateButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <button class="flex items-center justify-center text-white absolute inset-0 z-10 bg-black/30 hover:bg-black/60">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 lg:w-32 lg:h-32"
+            :class="iconClass"
+        >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+        </svg>
+        <slot></slot>
+    </button>
+</template>
+
+<script setup>
+import { defineProps, computed } from 'vue'
+
+const { direction } = defineProps(['direction'])
+
+const iconClass = computed(() => {
+    switch (direction) {
+        case 'left':
+            return 'transform rotate-180'
+        case 'up':
+            return 'transform -rotate-90'
+        case 'down':
+            return 'transform rotate-90'
+    }
+})
+</script>
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -2,28 +2,52 @@
     <div class="flex w-full h-screen max-h-screen flex-col border-2 border-black" v-if="items.length !== 0">
         <div class="flex bg-black h-20 md:h-32 lg:h-44 shrink-0">
             <div class="border-2 border-black w-20 md:w-32 lg:w-44 shrink-0"></div>
-            <div class="border-2 border-black grow">
-                <img @click="loadItem(items[0].id)" :src="items[0].image_src" alt="" class="w-full h-full object-cover object-bottom cursor-pointer opacity-70 hover:opacity-100">
+            <div class="border-2 border-black grow relative">
+                <navigate-button @click="loadItem(items[0].id)" direction="up"></navigate-button>
+                <img
+                    @click="loadItem(items[0].id)"
+                    :src="items[0].image_src"
+                    :alt="items[0].title"
+                    class="w-full h-full object-cover object-bottom"
+                />
             </div>
-            <div class="border-2 border-black  w-20 md:w-32 lg:w-44 shrink-0"></div>
+            <div class="border-2 border-black w-20 md:w-32 lg:w-44 shrink-0"></div>
         </div>
         <div class="flex bg-black grow">
-            <div class="border-2 border-black w-20 md:w-32 lg:w-44 shrink-0">
-                <img :src="items[1].image_src" @click="loadItem(items[1].id)" alt="" class="w-full h-full object-cover object-right cursor-pointer opacity-70 hover:opacity-100">
+            <div class="border-2 border-black w-20 md:w-32 lg:w-44 shrink-0 relative">
+                <navigate-button @click="loadItem(items[1].id)" direction="left"></navigate-button>
+                <img
+                    :src="items[1].image_src"
+                    :alt="items[1].title"
+                    class="w-full h-full object-cover object-right"
+                />
             </div>
-            <div class="border-2 border-black grow max-h-[calc(100vh-10rem)] md:max-h-[calc(100vh-16rem)] lg:max-h-[calc(100vh-22rem)]">
-                <img :src="items[2].image_src" @click="toggleDetail()" alt="" class="w-full h-full object-cover cursor-pointer">
+            <div
+                class="border-2 border-black grow max-h-[calc(100vh-10rem)] md:max-h-[calc(100vh-16rem)] lg:max-h-[calc(100vh-22rem)]"
+            >
+                <img
+                    @click="toggleDetail()"
+                    :src="items[2].image_src"
+                    :alt="items[2].title"
+                    class="w-full h-full object-cover cursor-pointer"
+                />
             </div>
-            <div class="border-2 border-black  w-20 md:w-32 lg:w-44 shrink-0">
-                <img :src="items[3].image_src" @click="loadItem(items[3].id)" alt="" class="w-full h-full object-cover object-left cursor-pointer opacity-70 hover:opacity-100">
+            <div class="border-2 border-black w-20 md:w-32 lg:w-44 shrink-0 relative">
+                <navigate-button @click="loadItem(items[3].id)" direction="right"></navigate-button>
+                <img :src="items[3].image_src" alt="" class="w-full h-full object-cover object-left" />
             </div>
         </div>
-        <div class="flex bg-black  h-20 md:h-32 lg:h-44 shrink-0">
+        <div class="flex bg-black h-20 md:h-32 lg:h-44 shrink-0">
             <div class="border-2 border-black w-20 md:w-32 lg:w-44 shrink-0"></div>
-            <div class="border-2 border-black grow">
-                <img :src="items[4].image_src" @click="loadItem(items[4].id)" alt="" class="w-full h-full object-cover object-top cursor-pointer opacity-70 hover:opacity-100">
+            <div class="border-2 border-black grow relative">
+                <navigate-button @click="loadItem(items[4].id)" direction="down"></navigate-button>
+                <img
+                    :src="items[4].image_src"
+                    :alt="items[4].title"
+                    class="w-full h-full object-cover object-top"
+                />
             </div>
-            <div class="border-2 border-black  w-20 md:w-32 lg:w-44 shrink-0"></div>
+            <div class="border-2 border-black w-20 md:w-32 lg:w-44 shrink-0"></div>
         </div>
     </div>
     <div v-if="isLoading" class="fixed inset-0 z-50 flex items-center justify-center">
@@ -37,6 +61,7 @@ import { ref, computed, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { getActiveLanguage, loadLanguageAsync } from 'laravel-vue-i18n'
 import ItemDetail from '../components/ItemDetail.vue'
+import NavigateButton from '../components/NavigateButton.vue'
 
 const router = useRouter()
 const route = useRoute()
@@ -55,7 +80,7 @@ onMounted(async () => {
     items.value = response.data.data
 })
 
-const loadItem = async(id) => {
+const loadItem = async (id) => {
     isLoading.value = true
     const response = await axios.get(`${apiUrl}?id=${id}`)
     items.value = response.data.data
@@ -65,5 +90,4 @@ const loadItem = async(id) => {
 const toggleDetail = () => {
     detailActive.value = !detailActive.value
 }
-
 </script>
