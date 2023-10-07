ba57fab1b0dbe5bf36969b2d8bd54ec92adde50b
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-10-07 23:08:26 +0200

    adjust item detail for mobile

--- resources/js/components/ItemDetail.vue
+++ resources/js/components/ItemDetail.vue
@@ -3,13 +3,13 @@
         <div class="bg-black opacity-70 absolute inset-0 cursor-zoom-out" @click="emit('close')" />
         <div class="max-h-full overflow-y-auto overflow-x-hidden p-4">
             <div class="relative w-full max-w-3xl bg-white rounded-xl">
-                <img :src="item.image_src" :alt="item.title" class="w-full rounded-t-xl max-h-[calc(100vh-34rem)] object-contain" />
+                <img :src="item.image_src" :alt="item.title" class="w-full rounded-t-xl max-h-[calc(100vh-20rem)] md:max-h-[calc(100vh-34rem)] object-contain" />
                 <div class="px-4 py-6 md:px-6">
-                    <h3 class="text-gray-dark text-2xl mb-1" v-if="item.object">
+                    <h3 class="text-gray-dark text-lg md:text-2xl mb-1" v-if="item.object">
                         {{ item.object }}
                     </h3>
-                    <h2 class="text-4xl font-bold  mb-1">{{ item.title }}</h2>
-                    <h3 class="text-gray-dark text-2xl">
+                    <h2 class="text-xl md:text-4xl font-bold  mb-1">{{ item.title }}</h2>
+                    <h3 class="text-gray-dark text-lg md:text-2xl">
                         <span v-if="item.author">{{ item.author }}</span>
                         <span v-if="item.author && item.dating"> · </span>
                         <span v-if="item.dating">{{ item.dating }}</span>
@@ -43,7 +43,7 @@
                             </div>
                         </div>
                     </a>
-                    <ConfirmButton class="bg-black text-white my-4 text-4xl" @click="emit('close')">Schließen</ConfirmButton>
+                    <ConfirmButton class="bg-black text-white my-2 md:my-4 text-lg md:text-2xl" @click="emit('close')">Schließen</ConfirmButton>
                 </div>
             </div>
         </div>
