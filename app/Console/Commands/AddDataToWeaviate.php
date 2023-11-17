da21ef92c06408e11daf6a0a7b8179eafaa4694e
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-11-17 14:31:38 +0100

    use chunk + batch to index all

--- app/Console/Commands/AddDataToWeaviate.php
+++ app/Console/Commands/AddDataToWeaviate.php
@@ -26,45 +26,52 @@ class AddDataToWeaviate extends Command
             "valueString" => "x"
         ]);
 
-        $items = Item::whereNotNull('asset_id')->get();
-        $totalItems = $items->count();
+        $itemsQuery = Item::whereNotNull('asset_id');
+        $totalItems = $itemsQuery->count();
 
         $progressBar = $this->output->createProgressBar($totalItems);
         $progressBar->start();
 
-        foreach ($items as $item) {
-            $imagePath = storage_path("app/images/{$item->id}.jpg");
+        $chunkSize = 100;
+        $itemsQuery->chunk($chunkSize, function ($items) use ($weaviate, $progressBar) {
+            $items_bulk = [];
+            foreach ($items as $item) {
+                $imagePath = storage_path("app/images/{$item->id}.jpg");
 
-            if (file_exists($imagePath)) {
-                try {
-                    $image = file_get_contents($imagePath);
-                    $imageBase64 = base64_encode($image);
+                if (file_exists($imagePath)) {
+                    try {
+                        $image = file_get_contents($imagePath);
+                        $imageBase64 = base64_encode($image);
 
-                    $object = $weaviate->dataObject()->create([
-                        'class' => $this->className,
-                        'properties' => [
-                            'identifier' => $item->id,
-                            'object' => $item->object,
-                            'title' => $item->title,
-                            'material' => $item->material,
-                            'technique' => $item->technique,
-                            'iconography' => $item->iconography,
-                            'style' => $item->style,
-                            'year' => $item->year,
-                            'collection' => $item->collection,
-                            'image' => $imageBase64,
-                        ]
-                    ]);
-                } catch (\Exception $e) {
-                    $this->error('An error occurred while adding data to Weaviate: ' . $e->getMessage());
-                    continue;
+
+                        // $object = $weaviate->dataObject()->create([
+                        $items_bulk[] = [
+                            'class' => $this->className,
+                            'properties' => [
+                                'identifier' => $item->id,
+                                'object' => $item->object,
+                                'title' => $item->title,
+                                'material' => $item->material,
+                                'technique' => $item->technique,
+                                'iconography' => $item->iconography,
+                                'style' => $item->style,
+                                'year' => $item->year,
+                                'collection' => $item->collection,
+                                'image' => $imageBase64,
+                            ]
+                        ];
+                    } catch (\Exception $e) {
+                        $this->error('An error occurred while adding data to Weaviate: ' . $e->getMessage());
+                        continue;
+                    }
                 }
+                $progressBar->advance();
             }
-            $progressBar->advance();
-        }
+            $weaviate->batch()->create($items_bulk);
+        });
 
         $progressBar->finish();
         $this->line('');
-        $this->info('Data for ' . $totalItems. ' objects has been added to ' . $this->className . ' class in Weaviate.');
+        $this->info('Data for ' . $totalItems . ' objects has been added to ' . $this->className . ' class in Weaviate.');
     }
 }
